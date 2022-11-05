d76e39cafdf39c3f40cd083018547497328bd2ba
Cody P <codypumper@gmail.com>
2022-11-05 13:03:52 -0700

    Update contract with assetId

--- packages/hardhat/contracts/HumbleOpinion.sol
+++ packages/hardhat/contracts/HumbleOpinion.sol
@@ -2,43 +2,72 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract HumbleOpinion {
-  struct Review {
-    uint id;
-    string text;
-    bool ownerWhenReviewed;
-    address author;
-    // string collectionHash;
-    string assetHash;
-    uint rating;
-  }
+    struct Review {
+        uint256 id;
+        string text;
+        bool ownerWhenReviewed;
+        address author;
+        string assetHash;
+        string assetId;
+        uint256 rating;
+    }
 
-  Review[] public reviews;
-  uint public reviewCount;
+    Review[] public reviews;
+    uint256 public reviewCount;
 
-  // Might be able to clean this out in the future
-  event NewReview(address sender, string review, uint rating, string assetHash);
+    // Might be able to clean this out in the future
+    event NewReview(
+        address sender,
+        string review,
+        uint256 rating,
+        string assetHash,
+        string assetId
+    );
 
-  constructor() {
-    // what should we do on deploy?
-  }
+    constructor() {
+        // what should we do on deploy?
+    }
 
-  function create(string memory _text, bool _owned, string memory _assetHash, uint _rating) external {
-      require(bytes(_text).length > 0, "Review text is empty");
-      require(bytes(_assetHash).length > 0, "Asset hash is empty");
-      require(msg.sender!=address(0), "Auther address does not exist");
-      reviewCount = reviewCount + 1;
-      Review memory newReview = Review(reviewCount, _text, _owned, msg.sender, _assetHash, _rating);
-      reviews.push(newReview);
+    function create(
+        string memory _text,
+        bool _owned,
+        string memory _assetHash,
+        string memory _assetId,
+        uint256 _rating
+    ) external {
+        require(bytes(_text).length > 0, "Review text is empty");
+        require(bytes(_assetHash).length > 0, "Asset hash is empty");
+        require(msg.sender != address(0), "Author address does not exist");
+        reviewCount = reviewCount + 1;
+        Review memory newReview = Review(
+            reviewCount,
+            _text,
+            _owned,
+            msg.sender,
+            _assetHash,
+            _assetId,
+            _rating
+        );
+        reviews.push(newReview);
 
-      emit NewReview(msg.sender, _text, _rating, _assetHash);
-  }
+        emit NewReview(msg.sender, _text, _rating, _assetHash, _assetId);
+    }
 
-  function get(uint _index) external view returns(uint id, string memory text, uint rating, string memory assetHash, address author) {
-      Review memory at_i = reviews[_index];
-      return (at_i.id, at_i.text, at_i.rating, at_i.assetHash, at_i.author);
-  }
+    function get(uint256 _index)
+        external
+        view
+        returns (
+            uint256 id,
+            address author,
+            string memory text,
+            uint256 rating,
+            string memory assetHash,
+            string memory assetId
+        )
+    {
+        Review memory at_i = reviews[_index];
+        return (at_i.id, at_i.author, at_i.text, at_i.rating, at_i.assetHash, at_i.assetId);
+    }
 }
